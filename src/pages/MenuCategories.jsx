b0a35934c15a027aace0b9b879efd8e6b80201f2
Rijo Sebastian <rijobdk@gmail.com>
2024-04-16 18:57:55 +0530

    Add datas to MenuCategories.jsx when data is fetching

--- src/pages/MenuCategories.jsx
+++ src/pages/MenuCategories.jsx
@@ -1,9 +1,23 @@
- 
-
+import { Link } from "react-router-dom";
 const MenuCategories = ({ categoryList }) => {
-
-    console.log(categoryList, "== categoryList");
-  return <div></div>;
+  let categoryCards = categoryList.map((category) => (
+    <Link
+      to={`/menu-item-details/${category.idCategory}`}
+      key={category.idCategory}
+    >
+      <div className="border border-green-400 rounded-lg p-3   ">
+        <img src={category.strCategoryThumb} loading="lazy" alt={category.strCategory} />
+        <h1 className="text-center uppercase mt-2 font-semibold md:text-lg">
+          {category.strCategory}
+        </h1>
+      </div>
+    </Link>
+  ));
+  return (
+    <div className="grid gap-3 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 m-5">
+      {categoryCards}
+    </div>
+  );
 };
 
-export default MenuCategories
+export default MenuCategories;
